698fd8e19837575c4ea8ab3083e5d3c15637eea8
meamruri <meammf@yandex.ru>
2019-04-25 23:38:47 +0300

    Initial Commit

---
+++ fence-task/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fence-task
+//
+//  Created by Rom Dr on 25/04/2019.
+//  Copyright © 2019 Rom Dr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
