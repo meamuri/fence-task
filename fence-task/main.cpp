bf96af7806e048a55e9d603a8a91697a86768f00
meamruri <meammf@yandex.ru>
2019-04-27 10:52:24 +0300

    initial commit

--- fence-task/main.cpp
+++ fence-task/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct IntPair {
+    int left;
+    int right;
+};
+
+IntPair getSizes(string const& input);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // input q and n
+    string input_sizes;
+    getline(cin, input_sizes);
+    IntPair sizes = getSizes(input_sizes);
+    int n = sizes.left;
+    int q = sizes.right;
+    cout << "DEBUG " << n << " " << q << " " << endl;
+    
+    // input array
     return 0;
+}
+
+IntPair getSizes(string const& input) {
+    size_t space_pos = input.find(" ");
+    int n = stoi(input.substr(0, space_pos));
+    int q = stoi(input.substr(space_pos, input.length() - 1));
+    return IntPair{n, q};
 }
